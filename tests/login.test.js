620bc0b16b5389c2467f29d0963846ead603d69d
Ramesh-com <ramesh@composio.dev>
2025-01-27 12:59:36 +0530

    Update login.test.js

--- tests/login.test.js
+++ tests/login.test.js
@@ -9,7 +9,7 @@ test.describe.serial('Login Test Suite', () => {
     let context;
     let page;
   test('should login successfully', async ()=> {
-    browser = await chromium.launch({ headless: false });
+    browser = await chromium.launch({ headless: true });
     context = await browser.newContext(); 
     page = await context.newPage();
     await page.goto("https://app.composio.dev");
